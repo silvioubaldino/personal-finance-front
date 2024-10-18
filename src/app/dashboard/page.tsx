36866baece13026ff3818637005c46eb4c7b5de5
sineto_meli <silvio.neto@mercadolivre.com>
2024-10-18 14:12:44 -0300

    Add wallet component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import FilterMonthsMode from "@/app/shared/components/filter/UI/FilterMonthsMode
 import Balance from "@/app/shared/components/balance/ui/balance";
 import Wallets from "@/app/shared/components/wallet/ui/wallet";
 import styles from './styles/dashboard.module.css'
+import Activity from "@/app/shared/components/activity/ui/activity";
 
 const DashboardPage = () => {
     return (
@@ -14,6 +15,7 @@ const DashboardPage = () => {
                 <Wallets/>
             </div>
             <div className={styles.dashboradBody}>
+                <Activity/>
             </div>
             <ApplicationMenu/>
         </div>
