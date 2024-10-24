a0001526d65758eb17e8bd98c89d1a9e58c4b613
sineto_meli <silvio.neto@mercadolivre.com>
2024-10-24 16:49:35 -0300

    Add categories page

---
+++ src/app/categories/components/subcategory-list.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+type SubCategory = {
+    id: number;
+    description: string;
+};
+
+type SubCategoryListProps = {
+    subCategories: SubCategory[];
+};
+
+const SubCategoryList: React.FC<SubCategoryListProps> = ({ subCategories }) => {
+    return (
+        <div>
+            <h2>Subcategories</h2>
+            <ul>
+                {subCategories.map(subCategory => (
+                    <li key={subCategory.id}>
+                        {subCategory.description}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default SubCategoryList;
