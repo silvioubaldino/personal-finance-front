a851d72fbb9dba0ef9e77511795216bec37df158
sineto_meli <silvio.neto@mercadolivre.com>
2024-10-29 01:06:13 -0300

    Correct getBalance request

--- src/app/shared/components/balance/ui/balance.tsx
+++ src/app/shared/components/balance/ui/balance.tsx
@@ -2,33 +2,26 @@
 import React, {useEffect, useState} from 'react';
 import styles from '../styles/balance.module.css';
 import {getBalance} from "@/services/api";
-
-// this value will be got from redux
-const getFrom = () => {
-    return '2021-01-01';
-}
-
-// this value will be got from redux
-const getTo = () => {
-    return '2023-12-30';
-}
+import {useMonth} from "@/app/shared/components/context/ui/MonthContext";
 
 const Balance = () => {
     const [balance, setBalance] = useState({ expense: 0, income: 0, period_balance: 0 });
-    let from = getFrom()
-    let to = getTo()
+    const { currentMonth } = useMonth();
+
     useEffect(() => {
         const fetchBalance = async () => {
             try {
-                const data = await getBalance(from, to);
-                setBalance(data);
+                if (currentMonth.from && currentMonth.to) {
+                    const data = await getBalance(currentMonth.from, currentMonth.to);
+                    setBalance(data);
+                }
             } catch (error) {
                 console.error('Failed to fetch balance:', error);
             }
         };
 
         fetchBalance();
-    }, [from, to]);
+    }, [currentMonth]);
     return (
         <div className={styles.container}>
             <div className={styles.valueGreen}>{balance.income.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</div>
