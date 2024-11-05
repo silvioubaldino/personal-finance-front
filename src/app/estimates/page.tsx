10ec9824408fff13256469d8ef12104574a54b23
sineto_meli <silvio.neto@mercadolivre.com>
2024-11-05 16:41:55 -0300

    Create simple menu

--- src/app/estimates/page.tsx
+++ src/app/estimates/page.tsx
@@ -1,5 +1,4 @@
 import styles from './styles/estimate.module.css'
-import LogoutButton from "@/app/shared/components/logout/ui/logout-button";
 import FilterMonthsMode from "@/app/shared/components/filter/UI/FilterMonthsMode";
 import Balance from "@/app/shared/components/balance/ui/balance";
 import EstimateList from "@/app/shared/components/estimate/ui/estimateList";
@@ -7,7 +6,6 @@ import EstimateList from "@/app/shared/components/estimate/ui/estimateList";
 const EstimatePage = () => {
     return (
         <div className={styles.pageContainer}>
-            <LogoutButton/>
             <FilterMonthsMode/>
             <div className={styles.balanceContainer}>
                 <Balance/>
